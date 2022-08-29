577fd2d08b82c41d6e46e1b9946e804fc3d16790
Deon Levon Dmello <80735760+deondmello16@users.noreply.github.com>
2022-08-29 09:43:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-alert("Welome to ToDo List \n ")
-let list=[]
-let command ="0"
-while(command!=="quit"|| command !== "q"){    
-    if (command === 'add'){
-        list.push(prompt("Enter the task"));
-        command = prompt("What would you like to do :").toLocaleLowerCase();
-    }if (command === 'remove'){
-        let loc = prompt("Enter the location of the ")
-    }
+// alert("Welome to ToDo List \n ")
+// let list=[]
+// let command ="0"
+// while(command!=="quit"|| command !== "q"){    
+//     if (command === 'add'){
+//         list.push(prompt("Enter the task"));
+//         command = prompt("What would you like to do :").toLocaleLowerCase();
+//     }if (command === 'remove'){
+//         let loc = prompt("Enter the location of the ")
+//     }
     
-}
-console.log("YOu have quit the application ");
+// }
+// console.log("YOu have quit the application ");
 
